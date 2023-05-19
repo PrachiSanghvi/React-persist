4426a1b8228b676a5fd4d64ec1067f6a39a50b8b
prachi <prachisanghvi62@gmail.com>
2023-05-19 09:38:03 +0530

    login-credential-persist-reatc

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import {persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// we might want to delay the rendering of our app’s UI 
+// until the persisted data is available in the Redux store.
+//  For that, Redux Persist includes the PersistGate component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+// import storageSession from 'reduxjs-toolkit-persist/lib/storage/session'
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+// from userReducer to persistedReducer, which is an enhanced reducer with configuration to persist the userReducer state to local storage. 
 export const store =  configureStore({
-  reducer: userReducer,
+  reducer: persistedReducer,
+  middleware:[thunk]
 })
+
+export const persistor = persistStore(store) 
+
+// for multiple reducers/combine reducers
+
+// const rootReducer = combineReducers({ 
+//   user: userReducer,
+//   notes: NotesReducer
+// })
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = configureStore({
+//   reducer: persistedReducer
+// })
